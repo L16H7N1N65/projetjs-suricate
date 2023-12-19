2325facd0032a70c9c98f761950ed70218ef96dd
unknown <linda.meghouche@gmail.com>
2023-12-19 13:29:28 +0100

    new file:   ProjetJS_suricate/images/snickers-berry-whip.jpg 	modified:   ProjetJS_suricate/index.html 	modified:   ProjetJS_suricate/scripts/project.js 	modified:   ProjetJS_suricate/style/style.css 	ProjetJS_suricate/images/snickers-butterscotch.jpg 	ProjetJS_suricate/images/snickers-caramelo-bacon.jpg 	ProjetJS_suricate/images/snickers-dark.jpg 	ProjetJS_suricate/images/snickers-hi-protein.jpg 	ProjetJS_suricate/images/snickers-kesar-pista.jpg 	ProjetJS_suricate/images/snickers-lime.jpg 	ProjetJS_suricate/images/snickers-limon.png 	ProjetJS_suricate/images/snickers-mousse-de-maracuja.jpg 	ProjetJS_suricate/images/snickers-oats.jpeg 	ProjetJS_suricate/images/snickers-original.png 	ProjetJS_suricate/images/snickers-peanut-brownie.png 	ProjetJS_suricate/images/snickers-peanutbutter.jpg 	ProjetJS_suricate/images/snickers-sabor-coco.jpg 	ProjetJS_suricate/images/snickers-spicy.jpg 	ProjetJS_suricate/images/snickers-white.png 	"g\302\253\303\244\303\263\302\236h\302\216\357\200\216j5\303\212\357\200\220\303\240\357\200\214p\357\200\213\303\205f\302\261\302\203\302\264\302\2100k\302\245\303\250P\357\200\226%\303\246\303\225\357\200\224\302\203\303\201\302\202\357\200\222\303\236{\302\250\303\225\303\260\303\236\303\203a\303\257=\302\252z" 	"p\303\241\302\205\177\303\262\302\216\303\277\344\241\223wo\306\211Y"

--- ProjetJS_suricate/scripts/project.js
+++ ProjetJS_suricate/scripts/project.js
@@ -1,5 +1,6 @@
 // === constants ===
 const MAX_QTY = 9;
+const MIN_QTY = 1;
 const productIdKey = "product";
 const orderIdKey = "order";
 const inputIdKey = "qte";
@@ -27,7 +28,7 @@ let createShop = function () {
 }
 
 /*
-* create the div.produit elment corresponding to the given product
+* create the div.produit element corresponding to the given product
 * The created element receives the id "index-product" where index is replaced by param's value
 * @param product (product object) = the product for which the element is created
 * @param index (int) = the index of the product in catalog, used to set the id of the created element
@@ -41,12 +42,12 @@ let createProduct = function (product, index) {
 	// build the h4 part of 'block'
 	block.appendChild(createBlock("h4", product.name));
 	
-	// /!\ should add the figure of the product... does not work yet... /!\ 
+	// // /!\ should add the figure of the product... does not work yet... /!\ 
 	block.appendChild(createFigureBlock(product));
 
-	// build and add the div.description part of 'block' 
+	// // build and add the div.description part of 'block' 
 	block.appendChild(createBlock("div", product.description, "description"));
-	// build and add the div.price part of 'block'
+	// // build and add the div.price part of 'block'
 	block.appendChild(createBlock("div", product.price, "prix"));
 	// build and add control div block to product element
 	block.appendChild(createOrderControlBlock(index));
@@ -56,7 +57,7 @@ let createProduct = function (product, index) {
 
 /* return a new element of tag 'tag' with content 'content' and class 'cssClass'
  * @param tag (string) = the type of the created element (example : "p")
- * @param content (string) = the html wontent of the created element (example : "bla bla")
+ * @param content (string) = the html content of the created element (example : "bla bla")
  * @param cssClass (string) (optional) = the value of the 'class' attribute for the created element
  */
 let createBlock = function (tag, content, cssClass) {
@@ -75,7 +76,12 @@ let createBlock = function (tag, content, cssClass) {
 * TODO : add the event handling, 
 *   /!\  in this version button and input do nothing  /!\  
 */
+<<<<<<< Updated upstream
 let createOrderControlBlock = function (index) {
+=======
+// Modify createOrderControlBlock to include event listeners for input and button LM
+let createOrderControlBlock = (index)=> {
+>>>>>>> Stashed changes
 	let control = document.createElement("div");
 	control.className = "controle";
 
@@ -85,6 +91,7 @@ let createOrderControlBlock = function (index) {
 	input.type = "number";
 	input.value = "0";
 
+<<<<<<< Updated upstream
 	// add input to control as its child
 	control.appendChild(input);
 	
@@ -93,12 +100,116 @@ let createOrderControlBlock = function (index) {
 	button.className = 'commander';
 	button.id = index + "-" + orderIdKey;
 	// add control to control as its child
+=======
+	// Add input event listener for quantity validation
+	input.addEventListener("input", ()=> {
+			let quantity = parseInt(input.value);
+			if (isNaN(quantity) || quantity < 0 || quantity > MAX_QTY) {
+					input.value = "0";
+			}
+			updateButtonState(button, quantity);
+	});
+
+	let button = document.createElement("button");
+	button.className = 'commander';
+	button.id = index + "-" + orderIdKey;
+
+// Add button click event listener
+// button.addEventListener("click", ()=> {
+// 	addToCart(index, parseInt(input.value));
+// 	input.value = "0";
+// 	updateTotal();
+// });
+
+	// Add button click event listener
+button.addEventListener("click",  ()=> {
+	let quantity = parseInt(input.value);
+	if (quantity > 0) {
+			addToCart(index, quantity);
+			input.value = "0";
+			updateTotal();
+	} else {
+
+			console.log("Quantity must be greater than 0 to add to cart.");
+	}
+});
+
+	// Append input and button to control
+	control.appendChild(input);//Added
+>>>>>>> Stashed changes
 	control.appendChild(button);
 	
 	// the built control div node is returned
 	return control;
 }
 
+<<<<<<< Updated upstream
+=======
+// Function to update button state based on quantity Check Again Tomorrow!
+let updateButtonState = function (button, quantity) {
+	button.disabled = quantity === 0;
+	button.style.opacity = quantity === 0 ? 0.25 : 1;
+				
+};
+
+// Function to add product to the cart
+let addToCart = (index, quantity)=> {
+	let product = catalog[index];
+
+	// Check if the product is already in the cart
+	let existingCartItem = document.getElementById("cartItem-" + index);
+	if (existingCartItem) {
+			// Update the quantity if the product is already in the cart
+			let currentQuantity = parseInt(existingCartItem.querySelector(".quantite").textContent);
+			quantity += currentQuantity;
+			if (quantity > MAX_QTY) {
+					quantity = MAX_QTY;
+			}
+			
+			existingCartItem.querySelector(".quantite").textContent = quantity;
+	} else {
+			// Create a new cart item
+			let cartItem = createBlock("div", "", "achat");
+			cartItem.id = "cartItem-" + index;
+
+			let itemName = createBlock("span", product.name, "nom");
+			let itemQuantity = createBlock("span", quantity, "quantite");
+
+			cartItem.appendChild(itemName);
+			cartItem.appendChild(itemQuantity);
+
+			// Add a delete button to remove the item from the cart
+			let deleteButton = document.createElement("button");
+			deleteButton.textContent = "Supprimer";
+			deleteButton.addEventListener("click", ()=> {
+					cartItem.remove();
+					updateTotal();
+			});
+			cartItem.appendChild(deleteButton);
+
+			// Append the cart item to the cart
+			document.querySelector("#panier .achats").appendChild(cartItem);
+	}
+	updateTotal();
+};
+
+// Function to update the total in the cart LM
+let updateTotal = () =>{
+	let cartItems = document.querySelectorAll("#panier .achat");
+	let total = 0;
+
+	cartItems.forEach(function (cartItem) {
+			let quantity = parseInt(cartItem.querySelector(".quantite").textContent);
+			let index = cartItem.id.split("-")[1];
+			let product = catalog[index];
+			total += quantity * product.price;
+	});
+	total = total.toFixed(2);
+	document.getElementById("montant").textContent = total;
+};
+
+// First point is to createFigureBlock (below LM) 
+>>>>>>> Stashed changes
 
 /*
 * create and return the figure block for this product
@@ -112,6 +223,20 @@ let createFigureBlock = function (product) {
 }
 
 
+<<<<<<< Updated upstream
 var imagejavascript = document. createElement("img");
 imagejavascript.src = "images/nounours1.jpg";
 document.body.appendChild(imagejavascript)
+=======
+	return figure;
+};
+
+// return createBlock("figure", `<img src="${product.image}">`, "lulu"); Study tonight and replicate
+
+
+// Remember to modify the input to only one number possible at time
+
+
+
+	
+>>>>>>> Stashed changes
