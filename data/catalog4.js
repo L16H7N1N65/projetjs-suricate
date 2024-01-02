6960151043f56a19e55169467bfb8b62e2aa967f
Chr1stopherPerez <150254591+Chr1stopherPerez@users.noreply.github.com>
2024-01-02 09:41:38 +0100

    Update catalog4.js

--- data/catalog4.js
+++ data/catalog4.js
@@ -1,119 +1,192 @@
 var catalog = [
-  {
-    "name": "Supreme x Nike Air More Uptempo",
-    "description": "Supreme x Nike Air More Uptempo",
-    "image": "images/supreme x Nike Air More Uptempo.jpg",
-    "price": "499",
-    "gltf": "images/uploads_file/uploads_files.gltf", 
-    
-  },
-	
 	{
-	    "name" : "Cacao Plaid",
-	    "description" : " Sneakers AIR FORCE 1 CACAO PLAID",
-	    "image" : "images/brown.jpg",
-	    "price" : "89"
+	    "name" : "SUPREME X NIKE AIR MORE UPTEMPO RED",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir & imitation de cuir,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/Supreme_x_Nike_Air_More_Uptempo_Red_1.jpg",
+		"gltf": "images/uploads_file/uploads_files.gltf", 
+	    "price" : "1629.00",
+		"color" : "Varsity Red",
+		"size" : "41"
 	},
 	{
-	    "name" : "Doernbecher",
-	    "description" : "Sneakers AIR JORDAN 3 DOERNBECHER",
-	    "image" : "images/retro.jpg",
-	    "price" : "259"
-	},	
-	{
-	    "name" : "Grey Fog",
-	    "description" : "Sneakers DUNK LOW GREY FOG",
-	    "image" : "images/grey.jpg",
-	    "price" : "89"
+	    "name" : "AIR JORDAN 1 MID SE",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir & imitation de cuir,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/628753_full_product.jpg",
+	    "price" : "137.95",
+		"color" : "Dark Chocolate / Crimson Bliss",
+		"size" : ["38", "39", "40", "41", "42"]
 	},
 	{
-	    "name" : "Chicago Lost & Found",
-	    "description" : "AIR JORDAN 1 CHICAGO LOST & FOUND",
-	    "image" : "images/red.jpg",
-	    "price" : "199"
+	    "name" : "NIKE W AIR FORCE 1 LXX",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir & imitation de cuir,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/815979_full_product.jpg",
+	    "price" : "130.00",
+		"color" : "Vachetta Tan / Flax-Sail",
+		"size" : ["38", "39", "40", "41", "42"]
 	},
 	{
-	    "name" : "Pine Green",
-	    "description" : "NIKE SB X AIR JORDAN 4 PINE GREEN",
-	    "image" : "images/pine.jpg",
-	    "price" : "309"
-	},
-	{
-	    "name" : "Palomino",
-	    "description" : "AIR JORDAN 1 PALOMINO",
-	    "image" : "images/palomino.jpg",
-	    "price" : "149"
-	},
-	{
-	    "name" : "Next Chapter",
-	    "description" : "MARVEL X AIR JORDAN 1 NEXT CHAPTER",
-	    "image" : "images/marvel.jpg",
-	    "price" : "129"
-	},
-	{
-	    "name" : "Cactus Jack",
-	    "description" : "TRAVIS SCOTT X DUNK LOW PREMIUM CACTUS JACK",
-	    "image" : "images/cactus.jpg",
-	    "price" : "999"
-	},
-	{
-	    "name" : "Zoom All Star",
-	    "description" : "ZOOM KOBE 6 ALL STAR",
-	    "image" : "images/zoom.jpg",
-	    "price" : "349"
-	},
-	{
-	    "name" : "Eybl",
-	    "description" : "KOBE 6 EYBL",
-	    "image" : "images/eybl.jpg",
-	    "price" : "1629"
-	},
-	{
-	    "name" : "Edge Camo",
-	    "description" : "REALTREE X JORDAN AIR SHIP EDGE CAMO",
-	    "image" : "images/camo.jpg",
-	    "price" : "139"
-	},
-	{
-	    "name" : "Rebellionaire",
-	    "description" : "AIR JORDAN 1 REBELLIONAIRE",
-	    "image" : "images/rebellionaire.jpg",
-	    "price" : "125"
-	},
-	{
-	    "name" : "University",
-	    "description" : "AIR FORCE 1 WHITE UNIVERSITY BLUE",
-	    "image" : "images/university.jpg",
-	    "price" : "69"
-	},
-	{
-	    "name" : "Sisterhood",
-	    "description" : "DUNK LOW SISTERHOOD - TEAM RED",
-	    "image" : "images/sisterhood.jpg",
-	    "price" : "89"
-	},
-	{
-	    "name" : "Space",
-	    "description" : "DUNK LOW UNLOCK YOUR SPACE",
-	    "image" : "images/space.jpg",
-	    "price" : "75"
-	},
-	{
-	    "name" : "Chi",
-	    "description" : "AIR JORDAN 1 NC TO CHI",
-	    "image" : "images/chi.jpg",
-	    "price" : "85"
-	},	
-    {
-	    "name" : "Swarovski",
-	    "description" : "CACTUS PLANT FLEA MARKET X SWAROVSKI X DUNK PURE PLATINUM",
-	    "image" : "images/swarovski.jpg",
-	    "price" : "3100"
+	    "name" : "NIKE AIR FORCE 1 MID '07 QS",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Dain & Synthétique,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/741091_full_product.jpg",
+	    "price" : "160.00",
+		"color" : "Chocolate/ Cream",
+		"size" : ["38", "39", "40", "41", "42"]
 	},
     {
-	    "name" : "Geode Teal",
-	    "description" : "DUNK LOW GEODE TEAL",
-	    "image" : "images/teal.jpg",
-	    "price" : "57"
+	    "name" : "NIKE DUNK FREDDY KRUEGER",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir & Textile,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/NIKE_DUNK_SB_FREDDY_KRUEGER_3.jpg",
+	    "price" : "182676.00",
+		"color" : "Rayures Rouges & Noires / Swoosh Argenté",
+		"size" : "43"
+	},
+	{
+	    "name" : "JORDAN 3 RETRO DOERNBECHER HUGO",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir et imitation de cuir,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/Doernbecher_1.jpg",
+	    "price" : "462.99",
+		"color" : "Pailleté dégoulinant  Vert",
+		"size" : ["38", "39", "40", "41", "42"]
+	},	
+	{
+	    "name" : "ADIDAS X PHARRELL WILLIAMS D.O.N. ISSUE 2",
+		"brand" : "ADIDAS",
+	    "description" : "Dessus / Tige: Tissu & Synthétique,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/452536_full_product.jpg",
+	    "price" : "72.49",
+		"color" : "Core Black",
+		"size" : "41"
+	},
+	{
+	    "name" : "ADIDAS HUMAN RACE NMD PHARRELL X CHANEL",
+		"brand" : "ADIDAS",
+	    "description" : "Dessus / Tige: Tissu & Synthétique,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/PharrellChanel_1.jpg",
+	    "price" : "21601.00",
+		"color" : "Core Black / Runing White",
+		"size" : "42"
+	},
+	{
+	    "name" : "AIR JORDAN 3 PINE GREEN",
+		"brand" : "NIKE",
+		"description" : "Dessus / Tige: Cuir,<br> Doublure: Textile, Semelle: Textile",
+		"image" : "images/Pine_Green_1.jpg",
+	    "price" : "335.00",
+		"color" : "Black / Silver / Pine Green",
+		"size" : ["38", "39", "40", "41", "42"]
+	},
+	{
+	    "name" : "AIR JORDAN 1 RETRO HIGH OG PALOMINO",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir et Textile,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/Air_Jordan_1_Retro_High_OG_Palomino_5.jpg",
+	    "price" : "189.99",
+		"color" : "Noir / Palomino",
+		"size" : "38"
+	},
+	{
+	    "name" : "MARVEL X AIR JORDAN 1 NEXT CHAPTER",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir et imitation de cuir,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/Air_Jordan_1_High_OG_Spider_Man_Across_the_Spider_Verse_1.jpg",
+	    "price" : "222.00",
+		"color" : "Bright Red",
+		"size" : ["38", "39", "40", "41", "42"]
+	},
+	{
+	    "name" : "OFF-WHITE X NIKE DUNK LOW LOT 50/50",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir et textile,<br> Doublure: Textile, Semelle de propreté: Textile",
+	    "image" : "images/Off_White_x_Nike_Dunk_Low_1.jpg",
+	    "price" : "703.00",
+		"color" : "Black/Metallic Silver-Black",
+		"size" : ["38", "39", "40", "41", "42"]
+	},
+	{
+	    "name" : "NIKE ZOOM KOBE 6 PROTRO MAMBACITA SWEET 16",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Tissu & Synthétique,<br> Doublure: Textile, Semelle de propreté: Textile",
+	    "image" : "images/Nike_Zoom_Kobe_6_Protro_Mambacita_Sweet_16_1.jpg",
+	    "price" : "1069.00",
+		"color" : "Black / White-Metallic Gold",
+		"size" : ["39", "40", "41", "42"]
+	},
+	{
+	    "name" : "NIKE MAC ATTACK WHITE YELLOW",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir, Mesh & Nylon,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/Nike_Mac_Attack_White_Yellow_1.jpg",
+	    "price" : "139.99",
+		"color" : "White / Yellow",
+		"size" : ["38", "39", "40", "41", "42"]
+	},
+	{
+	    "name" : "AIR JORDAN 1 RETRO HIGH OG REBELLIONAIRE",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir et Textile,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/Air_Jordan_1_Retro_High_OG_Rebellionaire_1.jpg",
+	    "price" : "280.00",
+		"color" : "Black / Grey",
+		"size" : ["38", "39", "40", "41", "42"]
+	},
+	{
+	    "name" : "NIKE MAG THE FUTURE IS BACK",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir et Synthétique,<br> Doublure: Synthétique, Semelle: Synthétique",
+	    "image" : "images/Nike_Mag_The_Future_Is_Back_1.jpg",
+	    "price" : "151793.00",
+		"color" : "Multi-color",
+		"size" : "47"
+	},
+	{
+	    "name" : "DIOR X AIR JORDAN 1 HIGH",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/Dior_x_Air_Jordan_1_High_1.jpg",
+	    "price" : "9943.00",
+		"color" : "Wolf Grey / Sail-Photon Dust-White",
+		"size" : ["41", "42"]
+	},
+	{
+	    "name" : "SNEAKER B23 HAUTE",
+		"brand" : "CONVESE",
+	    "description" : "Dessus / Tige: Toile,<br> Doublure: Tissu, Semelle: Caoutchouc ",
+	    "image" : "images/1592293528_3SH118YJP_H069_E03_ZH.jpg",
+	    "price" : "990.00",
+		"color" : "White / Grey",
+		"size" : ["38", "39", "40", "41", "42"]
+	},
+	{
+	    "name" : "CHAUSSURES VANS X ONE PIECE AUTHENTIC",
+		"brand" : "VANS",
+	    "description" : "Dessus / Tige: Toile,<br> Doublure: Tissu, Semelle: Caoutchouc",
+	    "image" : "images/vans_authenticstrawhatcrewnaturalmulti_shoes_skateshoes_vn0a5jmpkkk1_art22-006104_196570655013_532482-290_1.jpg",
+	    "price" : "58.50",
+		"color" : "Marron",
+		"size" : ["38", "39", "40", "41", "42"]
+	},
+    {
+	    "name" : "CACTUS PLANT FLEA MARKET X NIK DUNK LOW SWAROSKI",
+		"brand" : "NIKE",
+	    "description" : "Dessus / Tige: Cuir,<br> Doublure: Textile, Semelle: Textile",
+	    "image" : "images/Cactus_Plant_Flea_Market_x_Nike_Dunk_Low_Swarovski_1.jpg",
+	    "price" : "7924.00",
+		"color" : "Pure Platinum",
+		"size" : "40"
+	},
+    {
+	    "name" : "BASKETS 574 NEW BALANCE X MIU MIU EN DENIM MARBRÉ",
+		"brand" : "NEW BALANCE",
+	    "description" : "Dessus / Tige: Denim / Effilochée,<br> Doublure: Tissu, Semelle: Gomme",
+	    "image" : "images/BASKETS_574_NEW_BALANCE_X_MIU_MIU_EN_DENIM_MARBRÉ_1.jpg",
+	    "price" : "750.00",
+		"color" : "Cognac",
+		"size" : ["39", "40", "41"]
 	}
 ]
